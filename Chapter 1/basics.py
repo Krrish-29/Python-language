db86aad47f96fea4148ad43457b6814535c95f44
Krrish-29 <kingkrrish29@gmail.com>
2025-01-29 09:47:24 +0530

    PYTHON

---
+++ Chapter 1/basics.py
@@ -0,0 +1,16 @@
+print("hello world!")
+print("""l
+      p
+      l
+      l
+      l""")
+print('''l
+      l
+      p
+      l
+      l
+      l''')
+# single line coment
+"""multi 
+line 
+comment"""
---
+++ Chapter 2/Variables_datatypes.py
@@ -0,0 +1,8 @@
+a=1
+b=1.1
+c="Krrish"
+d=True
+e=None
+# variable name cannot start with numbers and no special characters
+# comparison operators return boolean 
+# logical operators 'or'  'and'  'not'
